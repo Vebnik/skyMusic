09540ed4b9405fe26a6da3f82b8c61a33ba05515
Vebnik(Nik) <vebnik111@gmail.com>
2022-04-26 18:12:53 +0300

    delete express

--- indexApp/app.js
+++ indexApp/app.js
@@ -2,13 +2,8 @@ const { Client, Intents } = require('discord.js');
 const {CreateApp} = require('../eventLogic/eventListeners.js')
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MESSAGES] });
 const app = new CreateApp(client)
-const express = require('express')
-const PORT = process.env.PORT || 80
-const appEx = express()
 
 
-appEx.get('/', (req, res) => { res.end('<h1>Start Page</h1>') })
-appEx.listen(PORT, () => console.log(`Start listen on ${PORT} PORT`))
 //
 app.listener(client)
 app.connect(client)
